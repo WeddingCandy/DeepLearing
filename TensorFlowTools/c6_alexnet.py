d690efec5d81647bd0ca07207750a6379fbfcb88
asus_pc <chenyouwenxyz@hotmail.com>
2018-11-06 17:03:36 +0800

    pc first submit

---
+++ TensorFlowTools/c6_alexnet.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+import math
+import time
+import tensorflow as tf
+
+batch_size = 32
+num_batches = 100
+
+def print_activations(t):
+    print(t.op.name, ' ', t.get_shape().as_list())
+
+
+
+
+def inference(images):
+    parameters = []
+
+    with tf.name_scope('conv1') as scope:
+        kernel = tf.Variable()
